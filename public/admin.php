ae4567d34e445c934521e8bfffc8784e5b0ac515
lmichael1996 <lmichael1996@users.noreply.github.com>
2026-01-10 15:21:45 +0100

    Separato stile admin in admin.css dedicato

--- public/admin.php
+++ public/admin.php
@@ -185,7 +185,7 @@ if ($selectedSetId) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - Marriage Game</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
     <div class="container">
