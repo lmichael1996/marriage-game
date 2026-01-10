6bd25a531b61be6b3d2faa47d790918976eea1f8
lmichael1996 <lmichael1996@users.noreply.github.com>
2026-01-10 15:19:39 +0100

    Separato stile login in login.css dedicato

--- public/login.php
+++ public/login.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Marriage Game</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <div class="container">
