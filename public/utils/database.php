7b7ba6963f2978785ae5a67bd7e9665fea1bd15c
lmichael1996 <lmichael1996@users.noreply.github.com>
2026-03-08 09:14:54 +0100

    style: database viewer restyled to match admin UI

--- public/utils/database.php
+++ public/utils/database.php
@@ -10,6 +10,7 @@ $tableData = $tableService->getAllTablesData();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Database - MVquiz</title>
+    <link rel="stylesheet" href="../../assets/css/base.css">
     <link rel="stylesheet" href="../../assets/css/database.css">
 </head>
 <body>
